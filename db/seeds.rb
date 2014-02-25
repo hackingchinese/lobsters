923d235eff6a25d3342ea9d67d8271c31379c89f
Stefan Wienert <stwienert@gmail.com>
2014-02-25 09:43:11 +0100

    seed categories updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ end
 [ [
   %w[ Beginner  Intermediate  Advanced  ], 0
 ], [
-  %w[ Listening Speaking Reading Writing Vocabulary Grammar Strategy Culture ], 1
+  %w[ Listening Speaking Reading Writing Vocabulary Grammar Culture Productivity], 1
 ], [
-  %w[ Blog-post Audio Video Book Website Software App ] ,2
+  %w[ Blog-post Audio Video Book Website Software Music Game Paid Dictionary Textbook News VPN-required ] ,2
 ]].each do |tag_group,tier|
   tag_group.each do |tag_name|
     tag = Tag.where(tag: tag_name).first_or_initialize
@@ -24,4 +24,6 @@ end
   end
 end
 Tag.where(tag: 'Software/app').destroy_all
+Tag.where(tag: 'App').update_all tag: 'Mobile'
+Tag.where(tag: 'Strategy').update_all tag: 'How-to-learn'
 
