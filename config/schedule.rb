3e59faf35408ce1bf6ff0ebeec338ca2dd151b02
Stefan Wienert <stwienert@gmail.com>
2014-03-05 12:57:29 +0100

    ts rebuild

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, "/apps/hackingchinese/prod/lobster/shared/log/cron.log"
 job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
 
 every 60.minutes do
-  rake "ts:index"
+  rake "ts:rebuild"
 end
 
