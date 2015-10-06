f57003ab59d825614939666045bb55dff7794071
Stefan Wienert <stefan.wienert@pludoni.de>
2015-10-06 15:49:23 +0200

    Fixed cronjob by making log relative

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :output, "/apps/hackingchinese/prod/lobster/shared/log/cron.log"
+set :output, "#{Dir.pwd}/log/cron.log"
 job_type :runner, "cd :path && bin/rails runner -e :environment ':task' :output"
 
 # every 60.minutes do
